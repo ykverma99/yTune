02e0382ad27c2b2ec752cb92a19904390f1e228e
ykverma99 <99yogeshwarkhurmi@gmail.com>
2024-09-04 22:32:18 +0530

    search page done

--- app/pages/search.tsx
+++ app/pages/search.tsx
@@ -1,19 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SearchInput from "@/components/SearchInput";
 import Title from "@/components/Title";
+import SongCard from "@/components/SongCard";
+import images from "@/constants/images";
 
 const search = () => {
   return (
-    <SafeAreaView className="flex-1 bg-secondary p-4 py-8">
+    <SafeAreaView className="flex-1 bg-secondary px-4 pt-8">
       <View className="flex-row items-center space-x-3">
         <SearchInput color="white" styleContainer="bg-white/20 w-[80%]" />
         <Text className="text-white">Cancel</Text>
       </View>
-      <View>
+      <ScrollView className="mt-6">
         <Title title="Recent searches" style="text-base" />
-      </View>
+        <View className="mt-4">
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+          <SongCard img={images.LongHair} title="Hozier" text="Artist" />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- components/SongCard.tsx
+++ components/SongCard.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageSourcePropType } from "react-native";
 import React from "react";
 
-const SongCard = () => {
+interface songType {
+  img: ImageSourcePropType;
+  imgStyle?: string;
+  styleContainer?: string;
+  titleStyle?: string;
+  textStyle?: string;
+  title: string;
+  text: string;
+}
+const SongCard = ({
+  img,
+  imgStyle,
+  title,
+  text,
+  styleContainer,
+  textStyle,
+  titleStyle,
+}: songType) => {
   return (
-    <View>
-      <Text>SongCard</Text>
+    <View className="flex-row items-center mt-3">
+      <View className="h-14 w-14 rounded-md mr-2">
+        <Image
+          source={img}
+          resizeMode="contain"
+          className={`h-full w-full rounded-md ${imgStyle}`}
+        />
+      </View>
+      <View>
+        <Text className={`text-white font-semibold`}>{title}</Text>
+        <Text className={`text-gray-400 text-xs`}>{text}</Text>
+      </View>
     </View>
   );
 };
